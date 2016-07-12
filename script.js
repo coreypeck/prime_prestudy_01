6cf07ffa37a430862d80587dd3a9e60fed1c45e7
coreypeck <coreypeck@gmail.com>
2016-07-11 21:11:15 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var working = function() {
+	var answer = confirm("Does this work?")
+		if (answer == true){
+			console.log("Hey this works!");
+		}
+		else{
+			console.log("What are you talking about? Of course this works!")
+		}
+}
